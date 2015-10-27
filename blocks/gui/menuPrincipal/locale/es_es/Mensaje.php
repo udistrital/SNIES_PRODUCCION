bf10efd416a2cdb5ca4273aba3971bdb68f778aa
fernando <fernando@localhost>
2015-10-27 18:56:43 -0500

    Bloque jóvenes en acción

--- blocks/gui/menuPrincipal/locale/es_es/Mensaje.php
+++ blocks/gui/menuPrincipal/locale/es_es/Mensaje.php
@@ -2,71 +2,30 @@
 $this->idioma ['noDefinido'] = 'No definido';
 
 $this->idioma ['inicio'] = 'Inicio';
-$this->idioma ['snies'] = 'SNIES';
-$this->idioma ['spadies'] = 'SPADIES';
-$this->idioma ['directiva_022'] = 'Directiva 022';
-$this->idioma ['registraduria'] = 'Registraduria';
 $this->idioma ['cerrarSesion'] = 'Cerrar Sesión';
 
-//SNIES///////////////////////////////////
-///INSCRITOS///
-$this->idioma ['tituloinscrito'] = 'inscrito';
+
+//SNIES
+$this->idioma ['snies'] = 'SNIES';
+
+$this->idioma ['tituloInscrito'] = 'Inscrito';
 $this->idioma ['actualizarsnieslocal'] = 'Actualizar SNIES LOCAL';
-	
-$this->idioma ['tituloadmitido'] = 'admitido';
-$this->idioma ['tituloestudiante'] = 'estudiante';
-$this->idioma ['titulograduado'] = 'graduado';
-$this->idioma ['titulodocente'] = 'docente';
-$this->idioma ['tituloenlacesnies'] = 'enlaces snies';
-$this->idioma ['reporteinformacion'] = 'Reportar información al SNIES CENTRAL';
-$this->idioma ['paginamesaayuda'] = 'Mesa de Ayuda SNIES';
-$this->idioma ['ticket'] = 'Tickets MEN';
-//Fin Hoja de Vida//////////////////////////////
+$this->idioma ['corregirnombre'] = 'Corrregir Nombres';
 
-//Asignación de Puntajes////////////////////////
-$this->idioma ['tituloSalariales'] = 'SALARIALES';
-$this->idioma ['produccionDeLibros'] = 'Producción de Libros';
-	//SALARIALES////////////////////////////////	
-	$this->idioma ['capituloLibros'] = 'Capítulos de Libros';
-	$this->idioma ['cartasEditor'] = 'Cartas al Editor';
-	$this->idioma ['direccionTrabajosGrado'] = 'Dirección de Trabajos de Grado';
-	$this->idioma ['experienciaDireccionAcademica'] = 'Experiencia Dirección Académica';
-	$this->idioma ['experienciaInvestigacion'] = 'Experiencia en Investigación';
-	$this->idioma ['experienciaDocencia'] = 'Experiencia en Docencia';
-	$this->idioma ['experienciaProfesional'] = 'Experiencia Profesional';
-	$this->idioma ['experienciaCalificada'] = 'Experiencia Calificada';	
-	$this->idioma ['excelenciaAcademica'] = 'Excelencia Académica';
-	$this->idioma ['revistasIndexadas'] = 'Revistas Indexadas';
-	$this->idioma ['comunicacionCorta'] = 'Comunicación Corta';
-	$this->idioma ['obrasArtisticas'] = 'Obras Artísticas';
-	$this->idioma ['patentes'] = 'Patentes';
-	$this->idioma ['PremiosDocente'] = 'Premios Docente';
-	$this->idioma ['produccionVideosDocente'] = 'Producción en Videos del Docente';
-	$this->idioma ['produccionLibros'] = 'Producción de Libros';
-	$this->idioma ['traducciones'] = 'Traducciones';
-	$this->idioma ['registroTecnicaSoftware'] = 'Registrar Técnica y Software Docente';
-	//Fin SALARIALES////////////////////////////////
-	
-$this->idioma ['tituloBonificacion'] = 'BONIFICACIÓN';		
-	//BONIFICACIÓN////////////////////////////////
-	$this->idioma ['publicacionesImpresas'] = 'Publicaciones Impresas Universitarias Docente';
-	$this->idioma ['estudiosPostdoctorales'] = 'Estudios PostDoctorales';
-	$this->idioma ['resenaCritica'] = 'Reseña Crítica';
-	$this->idioma ['traduccionArticulo'] = 'Traducción de Artículos del Docente';
-	$this->idioma ['ponenciasArticulos'] = 'Ponencias Docente';
-	//Fin BONIFICACIÓN////////////////////////////////
-	
-$this->idioma ['tituloNovedades'] = 'NOVEDADES';
-	//NOVEDADES////////////////////////////////	
-	$this->idioma ['novedades'] = 'Novedades';
-	//Fin NOVEDADES///////////////////////////////////	
-//Fin Asginación de Puntajes//////////////////////////
+$this->idioma ['admitido'] = 'Admitido';
+$this->idioma ['estudiante'] = 'Estudiante';
+$this->idioma ['graduado'] = 'Graduado';
+$this->idioma ['docente'] = 'Docente';
+$this->idioma ['enlaces'] = 'Enlaces';
 
-$this->idioma ['tituloConsultaReportes'] = 'CONSULTA REPORTES DOCENCIA';
-	//CONSULTA REPORTES DOCENCIA////////////////////////////////
-	$this->idioma ['consultaReportes'] = 'Gestor de Reportes';
-	$this->idioma ['estadoCuentaDocente'] = 'Estado de Cuenta Individual Docente';
-	
+//SNIES
+$this->idioma ['spadies'] = 'SPADIES';
+$this->idioma ['matriculado'] = 'Generar matriculado';
+
+//DPS Departamento de Prosperidad Social
+$this->idioma ['dps'] = 'DPS';
+$this->idioma ['jovenesenaccion'] = 'Jóvenes en Acción';
+$this->idioma ['generarreporte'] = 'Generar Reporte';
 
 $this->idioma ['usuario'] = 'Usuarios';
 $this->idioma ['sesion'] = 'Sesión';
